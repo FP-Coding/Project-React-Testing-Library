c680fab132d45dea017430bfa2b84a6c0d634264
Felipe Pinto <felipe14062000@gmail.com>
2022-09-19 16:05:59 -0300

    Resolução do 4 requisito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
+import renderWithRouter from './helpers/renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test(`Teste se a página contém um heading h2 
+  com o texto Page requested not found`, () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/qualquer-url');
+    });
+    const title = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(title).toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/qualquer-url');
+    });
+    const image = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    const srcImage = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe(srcImage);
+  });
+});
